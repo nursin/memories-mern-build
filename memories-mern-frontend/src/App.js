153e7fba8dcfda7eff14c7147d6938c5c65c1d3c
Bobby Keel <bobbykeel76@gmail.com>
2022-03-14 10:12:23 -0500

    setup frontend with material ui, axios setup, nice file structure in this tutorial

--- memories-mern-frontend/src/App.js
+++ memories-mern-frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import Posts from './components/Posts/Posts';
+import Form from './components/Forms/Form';
+import memories from './images/memories.png';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
