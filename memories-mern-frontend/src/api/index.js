58c214273ccde114ec373cd863cd61040de6a33e
Bobby Keel <bobbykeel76@gmail.com>
2022-03-17 18:55:22 -0500

    added axios api interceptor for jwt token to backend

--- memories-mern-frontend/src/api/index.js
+++ memories-mern-frontend/src/api/index.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:8000' });
+ 
+// need to send to backend middleware so it can verify we are actually logged in
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+        req.headers.Authorization = ` Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+    return req;
+});
 
 export const fetchPosts = () => API.get('/posts');
 export const createPosts = (newPost) => API.post('/posts', newPost);
